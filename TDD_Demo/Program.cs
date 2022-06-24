099c6ea7bbad2dc25c6a59a065ec9738f3e41595
Amoriss <amoriss@protonmail.com>
2022-06-23 23:53:54 -0500

    initial commit

---
+++ TDD_Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TDD_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //     Why test code???????
+
+            //    1. Ensure code works, under varying circumstances with different inputs
+            //    2. Improve quality 
+            //    3. Helps you focus on quality
+            //    4. Break code into smaller chucks
+            //    5. You add more error handling
+               
+
+            //ARRANGE
+            //initializes variables
+
+
+            //ACT 
+            //invokes method to test
+
+
+            //ASSERT
+            //verify the act
+
+
+            //TEST EARLY, TEST OFTEN!
+        }
+    }
+}
